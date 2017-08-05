bdd2d3a6ad8629ba2be9c9a98eddcdfdf8730d43
dirkstrauss <dirkstrauss@outlook.com>
2017-08-05 15:45:03 +0200

    Commit for Experts Exchange

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormsApp2;
+
+namespace ConsoleCallForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Going to start the WinForm");
+            Form1 myForm = new Form1();            
+            Application.Run(myForm);
+        }
+    }
+}
